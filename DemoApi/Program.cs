27f5c37e3d15fc171bddbcf42fdc6f371b972aa6
Simon McKenna <sshnug.si+github@gmail.com>
2023-02-09 21:15:38 +1030

    Add conditional development checks

--- DemoApi/Program.cs
+++ DemoApi/Program.cs
@@ -4,15 +4,23 @@ builder.Logging.AddConfiguration();
 
 builder.Configuration.AddEnvironmentVariables("DemoApi_");
 
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDatabasesInAssembly();
 builder.Services.AddHttpsRedirection(_ => { }); // Defaults to HTTPS_PORT environment variable
 builder.Services.ConfigureJsonOptions();
 
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+}
+
 var app = builder.Build();
 
-app.UseDeveloperExceptionPage();
 app.UseEndpointsInAssembly();
 app.UseHttpsRedirection();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
 
 app.Run();
